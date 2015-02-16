f3ca5d58bbe2a059b45ef384633377163975a00e
Mark Lintern <mark.lintern@oracle.com>
2015-02-16 15:22:25 -0500

    remove X-Frame-Option header

--- config/apps.rb
+++ config/apps.rb
@@ -26,7 +26,7 @@
 # override these settings in the subapps as needed.
 #
 Padrino.configure_apps do
-
+  set :protection, :except => :frame_options
 end
 
 # Mounts the core application for this project
