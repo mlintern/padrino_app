58fd64d6aea4c980ff9c27371f4434fff4679883
Mark Lintern <markllintern@gmail.com>
2015-04-01 20:25:52 -0400

    catch bad uri error for curl

--- app/controllers/curl.rb
+++ app/controllers/curl.rb
@@ -31,25 +31,29 @@ PadrinoApp::App.controllers :curl do
 
     @result = { :results => 'Did not make request' }
 
-    case params[:session]['call_type']
-      when "get"
-        if params[:session]['public'] == 'on'
-          params[:session]['only_curl'] == 'on' ? true : @result = @public.get(params[:session]['api_uri'],query)
-          curl_auth = ''
-        else
-          params[:session]['only_curl'] == 'on' ? true : @result = @compendium.get(params[:session]['api_uri'],query)
+    begin
+      case params[:session]['call_type']
+        when "get"
+          curl_call = curl_start+'"'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+norm_data(query)+'"'
+          if params[:session]['public'] == 'on'
+            params[:session]['only_curl'] == 'on' ? true : @result = @public.get(params[:session]['api_uri'],query)
+            curl_auth = ''
+          else
+            params[:session]['only_curl'] == 'on' ? true : @result = @compendium.get(params[:session]['api_uri'],query)
+          end
+        when "put"
+          curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XPUT'
+          params[:session]['only_curl'] == 'on' ? true : @result = @compendium.put(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
+        when "post"
+          curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XPOST'
+          params[:session]['only_curl'] == 'on' ? true : @result = @compendium.post(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
+        when "delete"
+          curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XDELETE'
+          params[:session]['only_curl'] == 'on' ? true : @result = @compendium.delete(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
         end
-        curl_call = curl_start+'"'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+norm_data(query)+'"'
-      when "put"
-        params[:session]['only_curl'] == 'on' ? true : @result = @compendium.put(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
-        curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XPUT'
-      when "post"
-        params[:session]['only_curl'] == 'on' ? true : @result = @compendium.post(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
-        curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XPOST'
-      when "delete"
-        params[:session]['only_curl'] == 'on' ? true : @result = @compendium.delete(params[:session]['api_uri'],body.to_s.is_json? ? body : body.to_json,query)
-        curl_call = curl_start+' --data \''+json_data(body)+'\' "'+params[:session]['protocol']+curl_auth+params[:session]['server']+params[:session]['api_uri']+'" -XDELETE'
-      end
+    rescue StandardError => e
+      logger.error(e)
+    end
 
     render 'curl/result', :locals => { :curl_call => curl_call, :result => @result }
 
