5347a10f3483b727a2df76d936980e2faf9b74bb
Mark Lintern <mark.lintern@oracle.com>
2016-03-08 15:23:29 -0500

    added missing function

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,3 +1,20 @@
+$.fn.serializeObject = function()
+{
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function() {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};
+
 function flashSuccess (message) {
 	$('.flash-notice').html('<div class="alert alert-success" role="alert">'+message+'</div>');
 	window.scrollTo(0,0);
