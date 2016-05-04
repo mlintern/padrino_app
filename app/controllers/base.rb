5a4268735ea7c6b94019244dcd4c86308d688ccc
Mark Lintern <mark.lintern@oracle.com>
2016-05-04 14:14:40 -0400

    /upload
    added /upload endpoint that echoes uploaded files

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,5 +1,13 @@
-PadrinoApp::App.controllers :base do
-  get :index, :map => "/" do
+include FileUtils::Verbose
+
+PadrinoApp::App.controllers :base, :map => "/" do
+
+  before do
+    headers 'Access-Control-Allow-Origin' => '*', 
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
+  end
+
+  get :index do
     if login(false)
       session[:redirect_to] = request.fullpath
       render "base/api_info"
@@ -7,4 +15,24 @@ PadrinoApp::App.controllers :base do
       render "base/index"
     end
   end
+
+  options :upload do
+    return 200
+  end
+
+  post :upload do
+    logger.debug params.inspect
+
+    response_hash = []
+    if params['files'] && params['files'].length > 0 && params['files'] != [""]
+      files = params['files']
+      files.each do |file|
+        name =  file[:filename]
+        size = File.size(file[:tempfile])
+        response_hash << { :name => name, :size => size.to_human(1) }
+      end
+    end
+    return 200, { :success => true, :files => response_hash }.to_json
+  end
+
 end
