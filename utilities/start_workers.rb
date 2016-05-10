758a3fdf6c763d635f13417086f80fdf965ada16
Mark Lintern <mark.lintern@oracle.com>
2016-05-10 14:30:18 -0400

    rubocop start_workers

--- utilities/start_workers.rb
+++ utilities/start_workers.rb
@@ -3,11 +3,11 @@
 
 require 'rubygems'
 
-puts "Spawning Worker"
+puts 'Spawning Worker'
 
 # Spawn Sidekiq worker inside the current process.
 worker_pid = spawn('bundle exec sidekiq -C ./config/sidekiq.yml -r ./config/boot.rb')
 
 Process.wait(worker_pid)
-puts "Worker died."
+puts 'Worker died.'
 Process.kill 'QUIT', Process.pid
