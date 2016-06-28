d7d4fccbaf1c7d4e42025a16f0c991636974e659
Mark Lintern <mark.lintern@oracle.com>
2016-06-28 14:53:08 -0400

    updated nretnil-editor

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -7,7 +7,29 @@ $(function() {
 		$('.editor').css({'color':color});
 	});
 
+	function insertTextAtCursor(text) {
+		var sel, range, html;
+		if (window.getSelection) {
+			sel = window.getSelection();
+			if (sel.getRangeAt && sel.rangeCount) {
+				range = sel.getRangeAt(0);
+				range.deleteContents();
+				range.insertNode( document.createTextNode(text) );
+			}
+		} else if (document.selection && document.selection.createRange) {
+			document.selection.createRange().text = text;
+		}
+	}
+
 	$('.editor').focus();
+
+	$('#nretnil-editor').keydown(function(e) {
+		console.debug(e);
+		if (e.key == "Tab" && e.keyCode == 9 ) {
+			e.preventDefault();
+			insertTextAtCursor('    ');
+		}
+	});
 
 	var cssElement = $('.editor');
 
